f96024a0f7ae7cbaa93312e9d1f2f6463224d901
Past-ret <82879000+Past-ret@users.noreply.github.com>
2022-04-01 20:50:49 +0300

    Add files via upload

---
+++ Skier.py
@@ -0,0 +1,12 @@
+#created by Yanshev
+P = float(input("Введите P: "))/100
+A = 10.0
+run = A
+day = 0
+while run <= 200:
+   A*=1+P
+   run+=A
+   day+=1
+print("Пробежал = ", run)
+print("Дней = ", day)
+input()
